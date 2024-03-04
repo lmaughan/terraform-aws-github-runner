e4b049a8ed53a37704c89c605a7947b59a3c6f8f
Niek Palm <niek.palm@philips.com>
2024-03-04 10:58:29 +0100

    adjust coverage gates

--- lambdas/functions/spot-termination-watcher/jest.config.ts
+++ lambdas/functions/spot-termination-watcher/jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   ...defaultConfig,
   coverageThreshold: {
     global: {
-      statements: 96,
+      statements: 95.91,
       branches: 95.65,
       functions: 100,
       lines: 95.83,
